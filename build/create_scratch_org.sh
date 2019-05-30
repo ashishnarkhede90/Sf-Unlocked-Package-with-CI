4565237f87bac8e4f21739916d4fa7dda26f3289
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-05-30 21:28:19 +0530

    testing

--- build/create_scratch_org.sh
+++ build/create_scratch_org.sh
@@ -1,3 +1,7 @@
 # CIRCLE_BRANCH is an env variable that refers to the Git branch currently being built
 echo "Creating a scratch org"
+pwd
+ls -l
+cd node_modules/sfdx-cli/bin/run
+ls -l
 sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
