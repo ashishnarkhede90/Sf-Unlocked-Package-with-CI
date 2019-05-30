286ac6365965c7e8f6916d183e8bfef6194f9c45
Ashish Narkhede <ashish.narkhede90@gmail.com>
2019-05-30 22:35:07 +0530

    testing

--- build/create_scratch_org.sh
+++ build/create_scratch_org.sh
@@ -1,8 +1,3 @@
 # CIRCLE_BRANCH is an env variable that refers to the Git branch currently being built
 echo "Creating a scratch org"
-pwd
-ls -l
-cd /usr/local/lib/node_modules
-ls -l
-/usr/local/lib/node_modules/sfdx-cli/bin/run force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
-#sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
+sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
--- build/install.sh
+++ build/install.sh
@@ -1,8 +1,3 @@
 #Install dependencies, SFDX CLI
 echo "Installing Dependencies... "
 sudo npm install -global sfdx-cli
-ls -l
-pwd
-cd /usr/local/lib/node_modules
-pwd
-ls -l
